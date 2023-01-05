e3864524405cf8997185ef747ec75c234121c3b7
Ruben Gutierrez <rubdeivis@gmail.com>
2023-01-04 22:06:20 -0400

    fix

--- functions/bot/bot.mjs
+++ functions/bot/bot.mjs
@@ -160,4 +160,17 @@ bot.on("document", async (ctx) => {
   }
 });
 
-bot.launch();
+exports.handler = async (event) => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return {
+      statusCode: 400,
+      body: "This endpoint is meant for bot and telegram communication",
+    };
+  }
+};
+
+// bot.launch();
